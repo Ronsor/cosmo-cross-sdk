2ce8dd81e89a1a16a2e81e01f2aabdb82780c682
Ronsor <ronsor@ronsor.pw>
2022-12-26 17:00:03 -0700

    Add required chdir into OpenSSL source directory

--- recipes/openssl.sh
+++ recipes/openssl.sh
@@ -11,6 +11,7 @@ wget https://www.openssl.org/source/openssl-3.0.7.tar.gz -O openssl-3.0.7.tar.gz
 tar xzf openssl-3.0.7.tar.gz || exit 1
 patch -p1 < openssl-3.0.7.patch || exit 1
 
+cd openssl-3.0.7 || exit 1
 CC=cosmocc CXX=cosmoc++ AR=cosmoar LD=cosmold MAKEDEPPROG=cosmocc \
   ./Configure linux-generic64 no-shared no-dso no-dynamic-engine no-afalgeng \
   -DL_ENDIAN -UHAVE_DLINFO --prefix="${COSMO_SYSROOT}" --openssldir="${COSMO_SYSROOT}" || exit 1
